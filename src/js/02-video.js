b860f4645395b2ac627b3e410367f4eabb7b63a2
MaksymHolovchuk <122921772+MaksymHolovchuk@users.noreply.github.com>
2023-07-02 15:13:46 +0300

    init task 2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'vimeoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+const updateTime = time => localStorage.setItem(CURRENT_TIME, time.seconds);
+player.on('timeupdate', throttle(updateTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const inputFormKey = localStorage.getItem(STORAGE_KEY);
+let inputForm = {};
+if (inputFormKey) {
+  inputForm = JSON.parse(inputFormKey);
+  form.email.value = inputForm.email;
+  form.message.value = inputForm.message;
+}
+function inputFormOn(evt) {
+  inputForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputForm));
+}
+function submitFormOn(evt) {
+  evt.preventDefault();
+  if (form.email.value === '' || form.message.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+  inputForm.email = form.email.value;
+  inputForm.message = form.message.value;
+  console.log(inputForm);
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  inputForm = {};
+}
+form.addEventListener('input', throttle(inputFormOn, 500));
+form.addEventListener('submit', submitFormOn);
