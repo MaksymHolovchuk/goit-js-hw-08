095a65a6f5e4592d960f70aebc68e337f6758d32
MaksymHolovchuk <122921772+MaksymHolovchuk@users.noreply.github.com>
2023-07-02 14:18:27 +0300

    init task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({ original, preview, description }) => 
+`<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
 console.log(galleryItems);
+
